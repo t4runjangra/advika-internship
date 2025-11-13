b81af15b910cf2f49c8bc2a79f44ba4479010b8f
tarun <tarunjangra1903@gmail.com>
2025-11-14 00:09:10 +0530

    removed next button and added animation

--- assign-1 quiz/script.js
+++ assign-1 quiz/script.js
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const questionText = document.getElementById('question-text');
     const choicesList = document.getElementById('choices-list');
     const startButton = document.getElementById('start-btn');
-    const nextButton = document.getElementById('next-btn');
     const restartButton = document.getElementById('restart-btn');
     const scoreDisplay = document.getElementById('score');
     const navbarContainer = document.getElementById('navbarContainer');
@@ -20,8 +19,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const user = document.getElementById('username');
     const usernameForm = document.getElementById('username-form');
     const usernameInput = document.getElementById('username-input');
-    const submitbtn = document.getElementById('submit')
-
     const hamIcon = document.getElementById('ham-icon');
     const hamburgerMenu = document.getElementById('hamburger-menu');
     const closeBtn = document.getElementById('close-hamburger');
@@ -55,73 +52,73 @@ document.addEventListener("DOMContentLoaded", () => {
             score: 1
         },
 
-        {
-            question: "Which CSS property is used to change the background color of an element?",
-            choices: [
-                "color",
-                "bgcolor",
-                "background-color",
-                "background"
-            ],
-            correctAnswerIndex: 2, // background-color
-            score: 1
-        },
-        {
-            question: "What does the 'C' in CSS stand for?",
-            choices: [
-                "Creative",
-                "Computer",
-                "Cascading",
-                "Colorful"
-            ],
-            correctAnswerIndex: 2, // Cascading
-            score: 1
-        },
-        {
-            question: "In CSS, what is the default value of the 'position' property?",
-            choices: [
-                "relative",
-                "fixed",
-                "static",
-                "absolute"
-            ],
-            correctAnswerIndex: 2, // static
-            score: 1
-        },
+        // {
+        //     question: "Which CSS property is used to change the background color of an element?",
+        //     choices: [
+        //         "color",
+        //         "bgcolor",
+        //         "background-color",
+        //         "background"
+        //     ],
+        //     correctAnswerIndex: 2, // background-color
+        //     score: 1
+        // },
+        // {
+        //     question: "What does the 'C' in CSS stand for?",
+        //     choices: [
+        //         "Creative",
+        //         "Computer",
+        //         "Cascading",
+        //         "Colorful"
+        //     ],
+        //     correctAnswerIndex: 2, // Cascading
+        //     score: 1
+        // },
+        // {
+        //     question: "In CSS, what is the default value of the 'position' property?",
+        //     choices: [
+        //         "relative",
+        //         "fixed",
+        //         "static",
+        //         "absolute"
+        //     ],
+        //     correctAnswerIndex: 2, // static
+        //     score: 1
+        // },
 
-        {
-            question: "Which keyword is used to declare a constant variable in JavaScript?",
-            choices: [
-                "var",
-                "let",
-                "const",
-                "constant"
-            ],
-            correctAnswerIndex: 2, // const
-            score: 1
-        },
-        {
-            question: "What is the correct way to write an IF statement in JavaScript?",
-            choices: [
-                "if i == 5 then",
-                "if (i == 5)",
-                "if i = 5",
-                "if i = 5 then"
-            ],
-            correctAnswerIndex: 1, // if (i == 5)
-            score: 1
-        },
-        {
-            question: "Which operator is used to check for both value and type equality in JavaScript?",
-            choices: [
-                "==",
-                "!=",
-                "===",
-                "="
-            ],
-            correctAnswerIndex: 2, // ===
-            score: 1
-        },
+        // {
+        //     question: "Which keyword is used to declare a constant variable in JavaScript?",
+        //     choices: [
+        //         "var",
+        //         "let",
+        //         "const",
+        //         "constant"
+        //     ],
+        //     correctAnswerIndex: 2, // const
+        //     score: 1
+        // },
+        // {
+        //     question: "What is the correct way to write an IF statement in JavaScript?",
+        //     choices: [
+        //         "if i == 5 then",
+        //         "if (i == 5)",
+        //         "if i = 5",
+        //         "if i = 5 then"
+        //     ],
+        //     correctAnswerIndex: 1, // if (i == 5)
+        //     score: 1
+        // },
+        // {
+        //     question: "Which operator is used to check for both value and type equality in JavaScript?",
+        //     choices: [
+        //         "==",
+        //         "!=",
+        //         "===",
+        //         "="
+        //     ],
+        //     correctAnswerIndex: 2, // ===
+        //     score: 1
+        // },
         {
             question: "Which method is used to remove the last element from a JavaScript array and return that element?",
             choices: [
@@ -152,44 +149,32 @@ document.addEventListener("DOMContentLoaded", () => {
     let userScore = JSON.parse(localStorage.getItem("userScore")) || []
     addingScore()
 
-    
+
     const localUserName = user.textContent = localStorage.getItem("username");
     if (localUserName) {
         usernameForm.classList.add('hidden')
         startButton.classList.remove('hidden')
         hamIcon.classList.remove('hidden')
         startQuiz()
-    }
-    else {
+    } else {
         user.textContent = "Guest"
         hamIcon.classList.add('hidden')
     }
-    
+
     hamIcon.addEventListener('click', () => {
         hamburgerMenu.classList.toggle('hidden')
-        
     })
-    
+
     closeBtn.addEventListener('click', () => {
         hamburgerMenu.classList.add('hidden')
     })
 
-    nextButton.addEventListener('click', () => {
-        currentQuestionIndex++
-        if (currentQuestionIndex < questions.length) {
-            showQuestion();
-        } else {
-            showResult();
-        }
-        
-    })
     clearBtn.addEventListener('click', () => {
         localStorage.removeItem("userScore")
         scoreHistory.innerHTML = '<p>No previous scores available.</p>';
-        
-        
+        userScore = []
     })
-    
+
     logout.addEventListener('click', () => {
         confirm("Are you sure!")
         localStorage.clear()
@@ -202,27 +187,21 @@ document.addEventListener("DOMContentLoaded", () => {
         if (!localStorage.getItem("username")) {
             localStorage.setItem("username", username);
             user.textContent = username
-            console.log("Username stored in localStorage.");
-        } else {
-            console.log("Username already exists in localStorage.");
         }
         if (username.length < 3) {
             alert('Username must be at least 3 characters long.')
             return
         }
-        console.log(localStorage.getItem("username"));
-        
         user.textContent = localStorage.getItem("username")
         usernameForm.classList.add('hidden')
         questionContainer.classList.remove('hidden')
         hamIcon.classList.remove('hidden')
         startButton.classList.remove('hidden')
-        
+
     })
+
     startButton.classList.add('hidden')
     startButton.addEventListener("click", startQuiz)
-
-    
 
 
     function startQuiz() {
@@ -230,31 +209,61 @@ document.addEventListener("DOMContentLoaded", () => {
         startButton.classList.add('hidden')
         resultContainer.classList.add('hidden')
         questionContainer.classList.remove('hidden')
+        shuffleArray(questions);
         showQuestion()
     }
 
     function showQuestion() {
-        nextButton.classList.add('hidden')
-        questionText.textContent = questions[currentQuestionIndex].question;
+        questionText.innerHTML = `
+            <div class="question-card">
+                <div class="question-circle">Q-${currentQuestionIndex + 1}/${questions.length} </div>
+               <div class="question-title">${questions[currentQuestionIndex].question}</div>
+            </div>`;
+
         choicesList.innerHTML = "" // clear previous question 
 
         questions[currentQuestionIndex].choices.forEach((choice, index) => {
             currentQuestion.innerHTML = `${currentQuestionIndex + 1} / ${questions.length}`
             const li = document.createElement('li')
             li.textContent = choice
-            li.addEventListener('click', () => selectAnswer(index))
+            li.addEventListener('click', () => selectAnswer(index, li))
             choicesList.appendChild(li)
         })
     }
 
-    function selectAnswer(index) {
+    function selectAnswer(index, liElement) {
         const correctAnswer = questions[currentQuestionIndex].correctAnswerIndex
         if (index == correctAnswer) {
             score++
+            liElement.classList.add('correct-option')
+            setTimeout(() => {
+                liElement.classList.remove('correct-option')
+                nextQuestion()
+            }, 600);
+        }
+        else {
+            const choices = Array.from(choicesList.children);
+            const correctLi = choices[correctAnswer];
+            correctLi.classList.add('correct-option');
+
+            liElement.classList.add('incorrect-option')
+            setTimeout(() => {
+                liElement.classList.remove('incorrect-option')
+                correctLi.classList.remove('correct-option');
+                nextQuestion()
+            }, 600);
         }
 
         userScoreCard.innerHTML = `${score}/${questions.length}`
-        nextButton.classList.remove("hidden")
+        Array.from(choicesList.children).forEach(li => li.style.pointerEvents = "none")
+    }
+    function nextQuestion() {
+        currentQuestionIndex++
+        if (currentQuestionIndex < questions.length) {
+            showQuestion()
+        } else {
+            showResult()
+        }
     }
     function showResult() {
         const history = {
@@ -265,7 +274,7 @@ document.addEventListener("DOMContentLoaded", () => {
         userScore.push(history)
         saveUser()
         addingScore()
-        
+
         questionContainer.classList.add('hidden')
         resultContainer.classList.remove('hidden')
         scoreDisplay.textContent = `${score} out of ${questions.length}`
@@ -278,7 +287,7 @@ document.addEventListener("DOMContentLoaded", () => {
         userScoreCard.innerHTML = `-/-`
         currentQuestion.innerHTML = `-/-`
     })
-    
+
     function addingScore() {
         if (userScore.length === 0) {
             scoreHistory.innerHTML = '<p>No previous scores available.</p>';
@@ -287,6 +296,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 `<p>Quiz ${i + 1}: ${s.Score} / ${s.Total}</p>`).join('');
         }
     }
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
     function saveUser() {
         localStorage.setItem("userScore", JSON.stringify(userScore))
     }
